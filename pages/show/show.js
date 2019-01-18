2f101a9eb122820ab041b4b9e1010d5e62b8587a
evanhasnoclue <wangkai199385@126.com>
2019-01-18 18:14:32 +0800

    created show page

---
+++ pages/show/show.js
@@ -0,0 +1,71 @@
+var Bmob = require('../../utils/Bmob-1.6.7.min.js')
+// pages/show/show.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    const query = Bmob.Query('Sports')
+    query.field('Fields')
+    query.relation('Fields').then(res => {
+      console.log(res);
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
