486989828fc95d2278200b3d34065014630092bd
evanhasnoclue <wangkai199385@126.com>
2019-01-21 20:37:45 +0800

    showpage add information

--- pages/show/show.js
+++ pages/show/show.js
@@ -23,6 +23,39 @@ Page({
       page.setData({
         sport: res[0]
       })
+    });
+    let bookings = [];
+    let reviews = [];
+    const query_fields = Bmob.Query('Fields')
+    query_fields.equalTo('sport_id','==',sport_id)
+    query_fields.find().then(res => {
+      console.log(2,res)
+      res.forEach(field => {
+        const query_slots = Bmob.Query('Booking_slots')
+        query_slots.equalTo('field_id','==',field.objectId)
+        query_slots.select('booking_id')
+        query_slots.find().then(res => {
+          console.log(3,res)
+          res.forEach(booking => {
+            if(bookings.includes(booking.booking_id.objectId)){
+
+            }else {
+            bookings.push(booking.booking_id.objectId);
+            }
+          })
+        })
+      })
+    })
+    page.setData({
+      bookings: bookings
+    })
+    page.data.bookings.forEach(booking => {
+      const query_reviews = Bmob.Query('Reviews')
+      query_reviews.equalTo('booking_id', '==', booking)
+      console.log(booking)
+      query_reviews.find().then(res => {
+        console.log(4, booking, res)
+      })
     })
   },
 
